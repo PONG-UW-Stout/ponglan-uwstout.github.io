1b3573d649be64c15a70e4ff2a8535bef44d993c
JALR13 <julia.jalr13@gmail.com>
2025-10-26 16:37:35 -0500

    Countdown & LAN Nav Linking
    Implemented basic countdown function (to continue to be worked on).  Added the correct page link for the LAN page to the nav bar.

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,26 @@ function load_nav(level = ""){
             <ul class="flex">
                 <a href="` + level + `about.html"><li class="header_button">About</li></a>
                 <a href="` + level + `sponsors.html"><li class="header_button">Sponsors</li></a>
-                <a href="` + level + `lan.html"><li class="header_button">LAN</li></a>
+                <a href="` + level + `lan/2025_spooky.html"><li class="header_button">LAN</li></a>
                 <a href="` + level + `info.html"><li class="header_button">Events/Info</li></a>
             </ul>
         </nav>
     </div> `;
+}
+
+function countdown(elementID, timeString ){
+    var targetDate = new Date(timeString).getTime();
+    
+    setInterval(function(){
+        var currentDate = new Date().getTime();
+
+        var distance = targetDate - currentDate;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById(elementID).innerHTML = days + " days, " + hours + " hours, " + minutes + " minutes, " + seconds + " seconds";
+    }, 1000);
 }
